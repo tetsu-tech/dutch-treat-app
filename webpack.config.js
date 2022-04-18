b48d16236a4c7ab8018125835c229252d8525817
dano <sf.ht.py1024@gmail.com>
2022-04-18 22:51:21 +0900

    setting-webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // 本番にデプロイするときはモード値を production に設定
+    // development に設定すると元のファイルとの関連性がわかるソースマップと一緒に出力される
+    mode: 'development',
+
+    // 環境によってはsrc/index.ts
+    entry: 'src/index.tsx',
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,    // 環境によっては/\.ts$/
+                use: 'ts-loader', // TypeScript用のloader
+            },
+        ],
+    },
+    resolve: {
+        // React × TypeScriptで使う可能性のある拡張子を全て記述
+        extensions: [
+            '.ts', '.js', '.tsx', '.jsx'
+        ],
+    },
+};
